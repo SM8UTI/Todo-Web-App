0692aceada1995b67264c9e7d1b39a8a853af1bb
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-05-13 13:39:30 +0530

    fixed

--- src/Components/SmallAddBtn.jsx
+++ src/Components/SmallAddBtn.jsx
@@ -4,11 +4,11 @@ import { motion as m } from "framer-motion";
 const SmallAddBtn = ({ setModalOpen }) => {
   return (
     <m.div
-      initial={{ opacity: 0, translateX: "150px" }}
+      initial={{ opacity: 0, translateX: "-150px" }}
       animate={{ opacity: 1, translateX: 0 }}
       transition={{ ease: "easeInOut", duration: 0.85 }}
       className={
-        "fixed bottom-[2rem] right-[2.5rem]  w-[80px] aspect-square bg-indigo-600 grid place-content-center rounded-full cursor-pointer hover:drop-shadow-glow   border-2 border-indigo-700 hover:border-2 hover:border-indigo-300 transition-all duration-300 z-[999] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
+        "fixed bottom-[2rem] left-[1rem] lg:left-[1.5rem]  w-[80px] aspect-square bg-indigo-600 grid place-content-center rounded-full cursor-pointer hover:drop-shadow-glow   border-2 border-indigo-700 hover:border-2 hover:border-indigo-300 transition-all duration-300 z-[999] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
       }
       onClick={() => setModalOpen(true)}
     >
