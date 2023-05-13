8488e5a2b58d166d63cac41f78ea8dd22648fd8b
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-05-13 13:33:11 +0530

    fixed

--- src/Components/SmallAddBtn.jsx
+++ src/Components/SmallAddBtn.jsx
@@ -4,15 +4,15 @@ import { motion as m } from "framer-motion";
 const SmallAddBtn = ({ setModalOpen }) => {
   return (
     <m.div
-      initial={{ opacity: 0, translateY: "-50px" }}
-      animate={{ opacity: 1, translateY: 0 }}
+      initial={{ opacity: 0, translateX: "150px" }}
+      animate={{ opacity: 1, translateX: 0 }}
       transition={{ ease: "easeInOut", duration: 0.85 }}
       className={
-        "fixed top-[5.5rem] left-2 xl:left-4 w-[60px] aspect-square bg-indigo-600 grid place-content-center rounded-lg cursor-pointer hover:drop-shadow-glow   border-2 border-indigo-700 hover:border-2 hover:border-indigo-300 transition-all duration-300 z-[999] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
+        "fixed bottom-[2rem] right-[2.5rem]  w-[80px] aspect-square bg-indigo-600 grid place-content-center rounded-full cursor-pointer hover:drop-shadow-glow   border-2 border-indigo-700 hover:border-2 hover:border-indigo-300 transition-all duration-300 z-[999] shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
       }
       onClick={() => setModalOpen(true)}
     >
-      <AiOutlinePlus className={"text-base "} />
+      <AiOutlinePlus className={"text-xl "} />
     </m.div>
   );
 };
